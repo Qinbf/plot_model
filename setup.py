c5f52ec9fb5626d8cf43cb15b6e9ec8db8941495
Qinbf <114479602@qq.com>
2020-03-18 20:50:44 +0800

    upgrade

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name = 'plot_model',      
+    version = '1.0',
+    author = 'QinBingFeng',        
+    author_email = '114479602@qq.com',
+    url = 'https://github.com/Qinbf/plot_model',
+    description = 'plot model'   
+    packages=find_packages()
+    )
