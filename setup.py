eb1b50139959b388befe27117ccbe423e9269479
Qinbf <114479602@qq.com>
2020-03-19 17:12:00 +0800

    add color

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
 from setuptools import find_packages
+from os import path
 
 here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
