428647c3b3ceaa8871c8d7b6065f70324e8e8c26
Qinbf <114479602@qq.com>
2020-03-19 17:11:14 +0800

    add color

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup
 from setuptools import find_packages
 
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name = 'plot_model',      
     version = '0.14',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author = 'QinBingFeng',        
     author_email = '114479602@qq.com',
     url = 'https://github.com/Qinbf/plot_model',
